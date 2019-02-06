7aa253ed6dff8fd10222464e2563d503b01f205d
Erce Kal <erce.kal@plentific.com>
2019-02-06 13:01:05 +0000

    uses fixture data

--- src/components/User.test.js
+++ src/components/User.test.js
@@ -1,33 +1,11 @@
 import React from 'react';
 import User from './User';
 import renderer from 'react-test-renderer';
+import data from '../fixtures/userData.json'
 
 it('renders correctly', () => {
-  const item = {
-      id: 1,
-      name: "Leanne Graham",
-      username: "Bret",
-      email: "Sincere@april.biz",
-      address: {
-      street: "Kulas Light",
-      suite: "Apt. 556",
-      city: "Gwenborough",
-      zipcode: "92998-3874",
-      geo: {
-      lat: "-37.3159",
-      lng: "81.1496"
-      }
-      },
-      phone: "1-770-736-8031 x56442",
-      website: "hildegard.org",
-      company: {
-      name: "Romaguera-Crona",
-      catchPhrase: "Multi-layered client-server neural-net",
-      bs: "harness real-time e-markets"
-      }
-    }
   const tree = renderer
-    .create(<User item={item} />)
+    .create(<User item={data[0]} />)
     .toJSON();
   expect(tree).toMatchSnapshot();
 });
